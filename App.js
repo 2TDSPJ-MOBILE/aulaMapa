697c7bc8205f3b3ed30cab672cfa40e425219c01
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-05-13 09:05:22 -0300

    Realizado a segunda parte do maps

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import RNPickerSelect from 'react-native-picker-select'
+
 
 //Importando a biblioteca de localização
 import * as Location from 'expo-location'
@@ -8,10 +10,17 @@ import * as Location from 'expo-location'
 //Importando a biblioteca do Mapa
 import MapView, { Marker } from 'react-native-maps';
 
+const nomeAlunos = [
+  { label: "Fernando", value: [37.42, -122.08] },
+  { label: "Henrique", value: "Henrique" },
+  { label: "Isabella", value: "Isabella" }
+]
 
 export default function App() {
+  const [alunoSelecionado, setAlunoSelecionado] = useState('')
+
   //Criando estado para armazenar as coordenadas geográficas(latitude e logintude)
-  const [location, setLocation] = useState(null)
+  const [location, setLocation] = useState('')
 
   //Criando estado para armazenar o endereço de acordo com a latitude e longitude
   const [address, setAddress] = useState(null)
@@ -50,22 +59,22 @@ export default function App() {
             const { status } = await Location.requestForegroundPermissionsAsync()
             setPermission(status)//Armazena o status da permissão
           })()
-          }}/>
+        }} />
       </View>
     )
   }
 
   //Função para exibir o endereço
-  const renderAddress = () =>{
-    if(!address) return <Text>Carregando endereço...</Text>
+  const renderAddress = () => {
+    if (!address) return <Text>Carregando endereço...</Text>
 
     const street = address?.street || 'Rua não encontrada'
     const city = address?.city || 'Cidade não encontrada'
-    const region  = address?.region || 'Estado não encontrado'
+    const region = address?.region || 'Estado não encontrado'
     const country = address?.country || 'País não encontrado'
     const postalCode = address?.postalCode || 'CEP não encontrado'
 
-    return(
+    return (
       <View>
         <Text>Endereço completo:</Text>
         <Text>Rua:{street}</Text>
@@ -79,20 +88,43 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-     <Text>Sua localização</Text>
-     {location?(
-      <>
-        <Text>Latitude:{location.latitude}</Text>
-        <Text>Logintude:{location.longitude}</Text>
-        <MapView>
-          <Marker 
-            
+      <Text>Sua localização</Text>
+      {location ? (
+        <>
+          <Text>Latitude:{location.latitude}</Text>
+          <Text>Logintude:{location.longitude}</Text>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+             latitudeDelta: 0.01,//Zoom vertical
+              longitudeDelta: 0.01//Zoom horizontal
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude
+              }}
+              title='Você está aqui.'
+            />
+          </MapView>
+          {renderAddress()}
+          <RNPickerSelect
+            items={nomeAlunos}
+            onValueChange={(value) => setAlunoSelecionado(value)}
+            placeholder={{ label: 'Escolha uma aluno...', value: null }}
           />
-        </MapView>
-      </>
-     ):(
-      <Text>Carregando localização...</Text>
-     )}
+
+          {alunoSelecionado ? (
+            <Text>Aluno selecionado:{alunoSelecionado}</Text>
+          ) : null}
+          {console.log(alunoSelecionado)}
+        </>
+      ) : (
+        <Text>Carregando localização...</Text>
+      )}
     </View>
   );
 }
@@ -103,4 +135,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center'
   },
+  map: {
+    width: '100%',
+    height: 400
+  }
 });
